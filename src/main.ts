af23269e1a046db05a089171f1b8a3aeac0932ba
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-04-01 14:56:01 +0900

    [Fix] body parser 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser'; // 추가
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Stripe Webhook만 raw body 받도록 설정
+  app.use('/webhooks/stripe', bodyParser.raw({ type: 'application/json' }));
+
+  // 전역 ValidationPipe 설정은 그대로 유지
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +17,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
