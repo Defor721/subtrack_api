592db01a678ef362a42bc2b29ad0db235741e1a2
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-04-01 17:55:05 +0900

    [Fix] 안되면 죽을게게

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -5,9 +5,12 @@ import {
   Body,
   UsePipes,
   ValidationPipe,
+  Options,
+  Res
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -22,5 +25,13 @@ export class UsersController {
   @UsePipes(ValidationPipe)
   async signup(@Body() dto: CreateUserDto) {
     return this.usersService.create(dto);
+  }
+  @Options()
+  handleOptions(@Res() res: Response) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://subtrack-front.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    return res.sendStatus(204);
   }
 }
